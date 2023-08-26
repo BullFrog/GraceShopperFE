66207aa0feecc9196bc2d92dc5be5a5c230e7bcd
James <>
2023-08-26 14:46:41 -0500

    set up functions for creating orders and selecting individual orders in api/orders.js

---
+++ src/api/Orders.js
@@ -0,0 +1,38 @@
+const API_URL = "http://localhost:3000/api";
+
+//array of [products] and user's token
+const createOrder = async (products, token) => {
+    try {
+        const request = await fetch(`${API_URL}/orders`, {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify(
+                products
+            )
+        })
+        const result = await request.json()
+        console.log(result)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+//needs orderId and uesr's token
+const getOrderById = async (orderId, token) => {
+    try {
+        const request = await fetch(`${API_URL}/orders/${orderId}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        const result = await request.json()
+        console.log(result)
+    } catch (error) {
+        console.error(error)
+    }
+}
+export {createOrder, getOrderById}
