af7d9b8c536df9b4ab7e201ecd3b417ee12cb460
Hunter Vassar <vassarhunter@gmail.com>
2023-08-28 20:36:55 -0400

    added products home page

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,93 @@
-const Product = () => {
-<div>Hello</div>
-}
-export default Product;
+import React from 'react';
+
+
+const productDetails = ({ product }) => {
+  return (
+    <div className="product-card">
+      <img src={product.image} alt={product.name} />
+      <h2>{product.name}</h2>
+      <p>{product.description}</p>
+      <p>Price: ${product.price}</p>
+      <button>Add to Cart</button>
+    </div>
+  );
+};
+
+const ProductsHomePage = () => {
+  const products = [
+    {
+      name: 'Product 1',
+      image: 'product1.jpg',
+      description: 'Description for Product 1',
+      price: ,
+    },
+    {
+      name: 'Product 2',
+      image: 'product2.jpg',
+      description: 'Description for Product 2',
+      price: ,
+    },
+    {
+      name: 'Product 3',
+      image: 'product2.jpg',
+      description: 'Description for Product 2',
+      price: ,
+    },
+    {
+      name: 'Product 4',
+      image: 'product2.jpg',
+      description: 'Description for Product 2',
+      price: ,
+    },
+    {
+      name: 'Product 5',
+      image: 'product2.jpg',
+      description: 'Description for Product 2',
+      price: ,
+    },
+    {
+      name: 'Product 6',
+      image: 'product2.jpg',
+      description: 'Description for Product 2',
+      price: ,
+    },
+    {
+      name: 'Product 7',
+      image: 'product2.jpg',
+      description: 'Description for Product 2',
+      price: ,
+    },
+    {
+      name: 'Product 8',
+      image: 'product2.jpg',
+      description: 'Description for Product 2',
+      price: ,
+    },
+    {
+      name: 'Product 9',
+      image: 'product2.jpg',
+      description: 'Description for Product 2',
+      price: ,
+    },
+    {
+      name: 'Product 10',
+      image: 'product2.jpg',
+      description: 'Description for Product 2',
+      price: ,
+    },
+    //add more products
+  ];
+
+  return (
+    <div className="products-homepage">
+      <h1>Welcome to Our Products</h1>
+      <div className="product-list">
+        {products.map((product, index) => (
+          <productDetails key={index} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductsHomePage;
