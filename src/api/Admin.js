79cc004f10b5a1fe73c78610979b54bd561abd1e
James <>
2023-08-31 22:29:13 -0500

    basic order page displayed, working on single order view

--- src/api/Admin.js
+++ src/api/Admin.js
@@ -19,12 +19,159 @@ const adminLogin = async (username, password) => {
     }
 }
 
-const getAdminProducts = async () => {
-    console.log("work in progress")
+const getAdminProducts = async (token) => {
+    try {
+        const request = await fetch(`${API_URL}/admin/products`, {
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        const result = await request.json()
+        return result
+    } catch (error) {
+        console.error(error)
+    }
 }
 
-const getAdminOrders = async () => {
-    console.log("work in progress")
+const getAdminSingleProduct = async (token, productId) => {
+     try {
+       const request = await fetch(`${API_URL}/admin/products/${productId}`, {
+         headers: {
+           "Content-type": "application/json",
+           'Authorization': `Bearer ${token}`,
+         },
+       });
+       const result = await request.json();
+       return result;
+     } catch (error) {
+       console.error(error);
+     }
+}
+ 
+const createProduct = async (token, newProduct) => {
+     try {
+       const request = await fetch(`${API_URL}/admin/products`, {
+        method: "POST", 
+        headers: {
+           "Content-type": "application/json",
+           'Authorization': `Bearer ${token}`,
+         },
+         body: JSON.stringify({
+            newProduct
+        })
+       });
+       const result = await request.json();
+       return result;
+     } catch (error) {
+       console.error(error);
+     }
 }
 
-export {adminLogin, getAdminProducts, getAdminOrders}
+const cycleCountProduct = async (token, productId, count) => {
+     try {
+       const request = await fetch(`${API_URL}/admin/products/count/${productId}`, {
+        method: "PATCH", 
+        headers: {
+           "Content-type": "application/json",
+           'Authorization': `Bearer ${token}`,
+         },
+         body: JSON.stringify({
+            count: count
+         })
+       });
+       const result = await request.json();
+       return result;
+     } catch (error) {
+       console.error(error);
+     }
+}
+
+const deactivateProduct = async (token, productId) => {
+    try {
+      const request = await fetch(`${API_URL}/admin/products/${productId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/json",
+          "Authorization": `Bearer ${token}`,
+        },
+      });
+      const result = await request.json();
+      return result;
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+const updateProduct = async (token, productId, updatedProduct) => {
+   try {
+     const request = await fetch(
+       `${API_URL}/admin/products/${productId}`,
+       {
+         method: "PATCH",
+         headers: {
+           "Content-type": "application/json",
+           'Authorization': `Bearer ${token}`,
+         },
+         body: JSON.stringify({
+           updatedProduct
+         }),
+       }
+     );
+     const result = await request.json();
+     return result;
+   } catch (error) {
+     console.error(error);
+   }
+}
+
+const getAdminOrders = async (token) => {
+  try {
+    const request = await fetch(`${API_URL}/admin/orders`, {
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    const result = await request.json()
+    return result
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+const getAdminSingleOrder = async (token, orderId) => {
+    try {
+      const request = await fetch(`${API_URL}/admin/orders/${orderId}`, {
+        headers: {
+          "Content-type": "application/json",
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+      const result = await request.json();
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+}
+
+const updateOrderStatus = async (token, orderId, orderStatus) => {
+    try {
+      const request = await fetch(`${API_URL}/admin/orders/${orderId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-type": "application/json",
+          'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            status: orderStatus
+        })
+      });
+      const result = await request.json();
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+}
+
+export {adminLogin, getAdminProducts, getAdminOrders, createProduct, getAdminSingleProduct, getAdminSingleOrder, updateOrderStatus, cycleCountProduct, deactivateProduct, updateProduct}
--- src/components/AdminProducts.jsx
+++ src/components/AdminProducts.jsx
@@ -1,8 +1,13 @@
+import { useState, useEffect } from "react";
 import AdminNewProductForm from "./AdminNewProductForm";
+import { getAdminProducts } from "../api/Admin";
+import AdminNavbar from "./AdminNavbar";
 
 const AdminProducts = () => {
+  const [products, setProducts] = useState([])
   return (
     <>
+    <AdminNavbar />
     <AdminNewProductForm />
     <p>admin products page</p>
     </>
